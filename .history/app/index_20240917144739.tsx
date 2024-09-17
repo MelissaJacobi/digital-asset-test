ccede81227baae5cbf78dc641e32687a5b08a8bf
MelissaJacobi <144720727+MelissaJacobi@users.noreply.github.com>
2024-09-17 16:13:35 -0700

    Revert "Lab3"

--- .history/app/index_20240917144739.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native';
-
-export default function Index() {
-  return <Text>This is my Index page</Text>;
-}
--- app/(tabs)/cheese.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native';
-
-export default function Cheese() {
-  return <Text>This is my "CHEESE" page </Text>;
-}
